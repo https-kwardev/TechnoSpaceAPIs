2f6806d47cd118060b6890a0a188ee38e7d4bed2
N-Nangamso <kwanelegoldden531@gmail.com>
2023-08-19 01:40:25 +0200

    initial upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TechnoSpaceAPIs.Controllers
+{
+    [Route("api")]
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        [Route("authenticate")]
+        public IActionResult Authenticate()
+        {
+            // Initiate Google authentication
+            return Challenge(new AuthenticationProperties { RedirectUri = Url.Action("GoogleCallback") }, "Google");
+        }
+
+        // Handle Google authentication callback
+        [Route("google-callback")]
+        public async Task<IActionResult> GoogleCallback()
+        {
+            var result = await HttpContext.AuthenticateAsync("Google");
+            if (!result.Succeeded)
+            {
+                return BadRequest("Authentication failed");
+            }
+
+            var email = result.Principal.FindFirst(ClaimTypes.Email)?.Value;
+            var name = result.Principal.FindFirst(ClaimTypes.Name)?.Value;
+            var surname = result.Principal.FindFirst(ClaimTypes.Surname)?.Value;
+
+            // You can now store this data in the session, generate a token, etc.
+
+            // Redirect back to the .NET Framework app with user data
+            return Redirect($"https://localhost:44394/WebForm1?email={email}&name={name}&surname={surname}");
+        }
+    }
+}
